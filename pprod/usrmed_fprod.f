76104134cbb086cb273c91fc99308f437a857415
EFTHYMIOPOULOS Ilias <efthymio@clueet.cern.ch>
2024-02-26 18:26:25 +0100

    update

--- pprod/usrmed_fprod.f
+++ pprod/usrmed_fprod.f
@@ -39,84 +39,38 @@
 *----------------------------------------------------------------------*
 *
       INCLUDE 'paprop.inc'
+
+      COMMON / MYSIMULATION / IFOCUSING
       
       LOGICAL LFIRST
       DATA  LFIRST /.TRUE./
 
-      SAVE LFIRST,NT2HEAD, NT2INSHLD, NT2COLL, NBEFB2, NNA61
-      SAVE NAIRAFTER, NB3TVAC, NBEFTAX
-
+      
+      SAVE LFIRST,NTARGET,NVOID
+      
       IF (LFIRST) THEN
          LFIRST = .FALSE.
-         CALL GEON2R("T2HEAD  ", NT2HEAD,IERR)
-         CALL GEON2R("T2INSHLD", NT2INSHLD, IERR)
-         CALL GEON2R("T2COLL  ", NT2COLL, IERR)
-         CALL GEON2R("B3TVac  ", NB3TVAC, IERR)
-         CALL GEON2R("AIRBEFB2", NBEFB2, IERR)
-         CALL GEON2R("AIRAFTER", NAIRAFTER, IERR)
-         CALL GEON2R("NA61exp ", NNA61, IERR)
-         CALL GEON2R("VXSS    ", NVXSS, IERR)
-         CALL GEON2R("BEFTAX  ", NBEFTAX,IERR)
-
+         CALL GEON2R("TARGET  ", NTARGET,IERR)
+         CALL GEON2R("VOID    ", NVOID, IERR)
+        
          PRINT *,"Entering USRMED routine",MREG,NEWREG
-         PRINT *,"Named regions : ",NT2HEAD, NT2INSHLD, NT2COLL, NBEFB2, NNA61
-         PRINT *,"                ",NBEFB2, NVXSS, NAIRAFTER, NBEFTAX
-
+         PRINT *,"Named regions : ",NVOID,NTARGET
+         PRINT *,"Configuration : IFOCUSING=",IFOCUSING
          WRITE(95,*) "* 1=IJ 2=PLA 3-5={Xx,Yy,Zz},6-8={TXX,TYY,TZZ}, 9=WEE"
       END IF
 
-*C --- keep only pions, muons & kaons
-*
-*      IF ( IJ.EQ.1 .OR. IJ.LT.-6 ) THEN
-*         WEE = ZEROZERO
-*         RETURN
-*      END IF
+      IF ( (MREG.EQ.NTARGET) .AND. (NEWREG.EQ.NVOID) ) THEN
 
-*C --- calculate particle momentum
-*
-*      IF ( PLA.LT.0.0D0 ) THEN
-*         EKIN = ABS(PLA)
-*      ELSE IF ( PLA.GT.0.0D0 ) THEN
-*         IF ( (PLA**2+AM(IJ)**2).GT.0.0 ) THEN
-*            EKIN = SQRT((PLA**2+AM(IJ)**2))-AM(IJ)
-*         ELSE
-*            EKIN = 0.0
-*         ENDIF
-*      ELSE
-*         RETURN
-*      END IF
-*      IF ( (EKIN+AM(IJ)**2-AM(IJ)**2).GT.0.0 ) THEN
-*         PMOMZ = SQRT((EKIN+AM(IJ))**2-AM(IJ)**2)/FLOAT(ICHRGE(IJ))
-*         PMOMA = SQRT((EKIN+AM(IJ))**2-AM(IJ)**2)/FLOAT(IBARCH(IJ))
-*      ELSE
-*         PMOMZ = 0.0
-*         PMOZA = 0.0
-*      ENDIF
+*     --- score the mesons leaving the target 
 
-      IF ( MREG.EQ.NT2HEAD .AND. NEWREG.EQ.NT2INSHLD ) THEN
-
-*     --- score particles T2 shielding area, surrounding the target
-         
-         IF (IJ.EQ.1 .OR. IJ.EQ.2 .OR. (IJ.GT.10 .AND. IJ.LE.16) ) THEN 
-            WRITE(95,1000) IJ, PLA, Xx,Yy,Zz,TXX,TYY,TZZ,WEE
-         ENDIF
-*     ELSE IF (MREG.EQ.NB3TVAC .AND. NEWREG.EQ.NBEFB2) THEN
-*  --- particles before degrader
-*
-*         WRITE(96,1000) IJ,ICHRGE(IJ),IBARCH(IJ),AM(IJ),EKSCO,PLA,
-*     +        PMOMZ,PMOMA,Xx,Yy,Zz,TXX,TYY,TZZ,WEE
-*      ELSE IF (MREG.EQ.NAIRAFTER .AND. NEWREG.EQ.NNA61) THEN
-
-C --- particles after degrader
-
-*         WRITE(97,1000) IJ,ICHRGE(IJ),IBARCH(IJ),AM(IJ),EKSCO,PLA,
-*     +        PMOMZ,PMOMA,Xx,Yy,Zz,TXX,TYY,TZZ,WEE
-*      ELSE IF (MREG.EQ.NVXSS .AND. NEWREG.EQ.NBEFTAX) THEN
-C --- particles beftax
-* 
-*         WRITE(98,1000) IJ,ICHRGE(IJ),IBARCH(IJ),AM(IJ),EKSCO,PLA,
-*     +        PMOMZ,PMOMA,Xx,Yy,Zz,TXX,TYY,TZZ,WE
-       
+         IF ( (IJ.EQ.13).OR.(IJ.EQ.14).OR.(IJ.EQ.15).OR.(IJ.EQ.16) ) THEN    
+             WRITE(95,1000) IJ, PLA, Xx,Yy,Zz,TXX,TYY,TZZ,WEE
+             IF ( IFOCUSING.EQ.1 ) THEN
+                TXX = ZERZER
+                TYY = ZERZER
+                TZZ = ONEONE-1.0e-9
+             END IF
+         END IF       
       END IF
       RETURN
  1000 FORMAT(1(1X,I4),8(1X,G14.7))
